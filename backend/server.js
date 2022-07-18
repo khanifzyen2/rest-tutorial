bb75d152caf23f7ece7664be3f30183a1763ffa0
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2022-07-18 14:15:39 +0700

    route file

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api/goal", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+app.use("/api/goal/", require("./routes/goalRoutes.js"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
